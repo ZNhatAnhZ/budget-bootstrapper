f94a5a6184b2f9bcfd1647dd7c0dfbd07e344959
ZNhatAnhZ <ntnhatanh2001@gmail.com>
2025-08-07 19:16:37 +0700

    init project

---
+++ src/test/java/com/budgetbootstrapper/BudgetBootstrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetbootstrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBootstrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
